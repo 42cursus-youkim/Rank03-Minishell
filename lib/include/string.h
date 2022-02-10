2fec9c13315edb5f493c814975787ebf47c8395c
Hyojeong Kim <42.4.hyojekim@gmail.com>
2022-02-11 01:05:56 +0900

    feat: :sparkles: Add ft_strchr_i()
    문자열 속 특정 문자의 위치를 찾는 함수
    
    #28

--- lib/include/string.h
+++ lib/include/string.h
@@ -10,6 +10,10 @@ t_res	ft_arr_extend(char **parr[], char *src[]);
 int		ft_arr_len(char **arr);
 void	del_arr(char **arr);
 /*
+** < split.c > */
+
+char	**new_str_split(char const *str, char delimitor);
+/*
 ** < join.c > */
 
 char	*new_str_join(char **arr, char delimiter);
@@ -19,12 +23,9 @@ char	*new_str_join(char **arr, char delimiter);
 char	*new_str(const char *src);
 int		ft_str_append(char **pstr, char *src);
 /*
-** < split.c > */
-
-char	**new_str_split(char const *str, char delimitor);
-/*
 ** < util.c > */
 
 int		ft_strlen(const char *str);
-bool	is_str_equel(const char *str1, const char *str2);
+bool	is_str_equal(const char *str1, const char *str2);
+int		ft_strchr_i(const char *str, int c);
 #endif
--- lib/src/string/util.c
+++ lib/src/string/util.c
@@ -26,3 +26,19 @@ bool	is_str_equal(const char *str1, const char *str2)
 			return (false);
 	return (true);
 }
+
+int	ft_strchr_i(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+			return (i);
+		i++;
+	}
+	if ((char)c == '\0')
+		return (i);
+	return (ERR);
+}
