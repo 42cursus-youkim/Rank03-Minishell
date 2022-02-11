c9c533519c6fda9b0c1e533334ec2b7f4df7b7b4
scarf <greenscarf005@gmail.com>
2022-02-11 11:21:30 +0900

    fix: :bug: Fix header include error
    - 컴파일 시 라이브러리를 인자 목록 맨 뒤로 옮김
    - 여전히 `libft.h`가 `readline` 위에 있어야 하는 것은 변함 없음
    
    Fixes #30

--- include/minishell.h
+++ include/minishell.h
@@ -1,19 +1,24 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-// for window
+# ifdef WIN32
+#  include <sys/dirent.h>
+# else
+#  include <dirent.h>
+# endif
+
 # include <sys/stat.h>
 # include <fcntl.h>
 
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/types.h>
-# include <sys/dirent.h>
+
+# include "libft.h"
 
 # include <readline/readline.h>
 # include <readline/history.h>
 
-# include "libft.h"
 // # include "get_next_line.h"
 
 #endif
