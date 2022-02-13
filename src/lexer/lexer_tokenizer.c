5c2b2dfd65db1ea9fa6cb194824398088f1251f7
Hyojeong Kim <42.4.hyojekim@gmail.com>
2022-02-14 01:28:20 +0900

    feat: :sparkles: Improve lexer_tokenizer
    tokenizer 문자열 처리 기능 추가
    - `"`과 `'`로 감싸진 문자열이 중간에 있어도 처리 가능
    - 따옴표 없는 expansion 문자열(환경변수)도 parser에서 구분할 수 있게 처리
    
    closes #36

--- src/lexer/lexer_tokenizer.c
+++ src/lexer/lexer_tokenizer.c
@@ -12,16 +12,37 @@ t_AST_type	tokentype_check(const char *str)
 	return (WORD);
 }
 
-bool	is_quotes(const char *str)
+int	quotes_index(const char *str)
 {
-	if (str[0] == '\'' || str[0] == '\"')
-		return (true);
-	return (false);
+	const int	single_i = ft_strchr_i(str, '\'');
+	const int	double_i = ft_strchr_i(str, '\"');
+
+	if (single_i != ERR && double_i != ERR)
+	{
+		if (single_i < double_i)
+			return (single_i);
+		else
+			return (double_i);
+	}
+	if (single_i != ERR)
+		return (single_i);
+	if (double_i != ERR)
+		return (double_i);
+	return (ERR);
 }
 
 bool	is_expand_parameter(const char *str)
 {
-	if (str[0] == '\"' && (ft_strchr_i(str, '$') != ERR))
+	const int	expand_i = ft_strchr_i(str, '$');
+	const int	single_i = ft_strchr_i(str, '\'');
+	const int	start_i = ft_strchr_i(str, '\"');
+	int			end_i;
+
+	if (start_i == ERR || expand_i == ERR
+		|| (single_i != ERR && start_i > single_i))
+		return (false);
+	end_i = ft_strchr_i(&str[start_i + 1], '\"') + start_i + 1;
+	if (expand_i > start_i && expand_i < end_i)
 		return (true);
 	return (false);
 }
@@ -35,8 +56,8 @@ void	tokens_print(t_token tokens[])
 
 	i = -1;
 	while (tokens[++i].text)
-		printf("[%2d] " BGRN "%-12s" END "%s\n" END,
-			i, tokens[i].text, type_str[tokens[i].type]);
+		printf("[%2d] %-12s" BGRN "\t%s\n" END,
+			i, type_str[tokens[i].type], tokens[i].text);
 }
 
 void	del_tokens(t_token tokens[])
