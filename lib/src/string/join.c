98fb492531a24fdc3cee0acb722d89e4863a8101
youkim <42.4.youkim@gmail.com>
2022-02-17 16:55:10 +0900

    refactor: :recycle: Refactor string library
    - use ft_str_append on new_str_join
    - NULL handling for ft_str_append
    - remove stale comment
    - move some functions to util2

--- lib/src/string/join.c
+++ lib/src/string/join.c
@@ -1,22 +1,9 @@
 #include "libft.h"
 
-static char	*new_char_tostr(char c)
-{
-	char	*new;
-
-	new = malloc(2 * sizeof(char));
-	if (!new)
-		return (NULL);
-	new[0] = c;
-	new[1] = '\0';
-	return (new);
-}
-
 char	*new_str_join(char **arr, char delimiter)
 {
 	int			i;
 	char		*new;
-	char		*delim;
 	const int	arr_len = ft_arr_len(arr);
 
 	if (!arr || !arr_len)
@@ -24,15 +11,13 @@ char	*new_str_join(char **arr, char delimiter)
 	new = new_str("");
 	if (!new)
 		return (NULL);
-	delim = new_char_tostr(delimiter);
 	i = -1;
 	while (++i < arr_len - 1)
 	{
 		ft_str_extend(&new, arr[i]);
-		ft_str_extend(&new, delim);
+		ft_str_append(&new, delimiter);
 	}
 	ft_str_extend(&new, arr[i]);
-	free(delim);
 	return (new);
 }
 
