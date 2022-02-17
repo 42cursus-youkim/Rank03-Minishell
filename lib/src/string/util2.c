44aeb9042abdd0e80cca0c52b7b0dd99656b91e5
scarf <greenscarf005@gmail.com>
2022-02-17 13:58:02 +0900

    fix: :bug: Fix typo
    yes.

--- lib/src/string/util2.c
+++ lib/src/string/util2.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-//	replace str_p with new. new will not be allocated, it's just movced
+//	replace str_p with new. new will not be allocated, it's just moved
 t_res	ft_str_replace(char **str_p, char *new)
 {
 	if (!str_p || !new)
