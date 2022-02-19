edd5fe7dd4fb300b9e3acd1140ac514adda07072
scarf <greenscarf005@gmail.com>
2022-02-19 17:55:50 +0900

    fix: :bug: Child proc is now a function (that does not crash!)

--- src/api/exec.c
+++ src/api/exec.c
@@ -26,30 +26,45 @@ static void	api_exec_cmd_inner(t_AST_COMMAND *cmd, t_dict *env)
 static t_res	parent_proc(pid_t pid)
 {
 	int	status;
-	int	exitcode;
+	// int	exitcode;
 
-	printf(GRN "I'm parent and waiting for child\n" END);
-	waitpid(pid, &status, WNOHANG);
-	exitcode = api_handle_status(status);
-	if (exitcode == OK)
-	{
-		printf(GRN "child is successfully dead!\n" END);
-		return (OK);
-	}
-	else
-	{
-		printf(BRED "exitcode: %d\n" END, exitcode);
-		return (ERR);
-	}
-}
+	// printf(GRN "I'm parent and waiting for child\n" END);
+	// waitpid(pid, &status, WNOHANG);
+	// exitcode = api_handle_status(status);
+	// if (exitcode == OK)
+	// {
+	// 	printf(GRN "child is successfully dead!\n" END);
+	// 	return (OK);
+	// }
+	// else
+	// {
+	// 	printf(BRED "exitcode: %d\n" END, exitcode);
+	// 	return (ERR);
+	// }
 
-static int	child_proc(t_AST_COMMAND *cmd, t_dict *env)
-{
-	printf("HAYO I'm child\n");
-	api_exec_cmd_inner(cmd, env);
-	return (0);
 }
 */
+
+static void	child_proc(t_AST_COMMAND *cmd, t_dict *env)
+{
+	(void)env; (void)cmd;
+
+	printf(HYEL "HAYO I'm child\n" END);
+	char *argv[] = {"/bin/ls", "-l", "/home/scarf/Repo", NULL};
+	char *envp[] = {NULL};
+	if (execve(argv[0], argv, envp) == -1)
+		printf("execve failed\n");
+	exit(0);
+}
+
+// static void	parent_proc(pid_t pid)
+// {
+// 	int	status;
+// 	printf(HGRN "HAYO I'm Parent process\n" END);
+// 	waitpid(pid, &status, WNOHANG);
+// 	printf ("Child process exited with status %d\n", api_handle_status(status));
+// }
+
 t_res	api_exec_cmd(t_AST_COMMAND *cmd, t_dict *env)
 {
 	pid_t	pid;
@@ -60,26 +75,29 @@ t_res	api_exec_cmd(t_AST_COMMAND *cmd, t_dict *env)
 
 	if (is_child(pid))
 	{
-		printf("Child process\n");
-		char *argv[] = {"/bin/ls", "-l", "/home/scarf/Repo", NULL};
-		char *envp[] = {NULL};
-		if (execve(argv[0], argv, envp) == -1)
-			printf("execve failed\n");
+		child_proc(NULL, NULL);
+		// char *argv[] = {"/bin/ls", "-l", "/home/scarf/Repo", NULL};
+		// char *envp[] = {NULL};
+		// if (execve(argv[0], argv, envp) == -1)
+		// 	printf("execve failed\n");
 	}
 	else if (is_parent(pid))
 	{
-		printf("Parent process\n");
-		int status;
+		// parent_proc(pid);
+		int	status;
+		printf(HGRN "HAYO I'm Parent process\n" END);
 		waitpid(pid, &status, WNOHANG);
 		printf ("Child process exited with status %d\n", api_handle_status(status));
+		return (OK);
 	}
-	return (0);
+	else
+		printf(RED "fork error\n" END);
+	return (OK);
 	// if (is_parent(pid))
 	// 	return (parent_proc(pid));
 	// else if (is_child(pid))
 	// 	return (child_proc(cmd, env));
 	// else
-	// 	printf(RED "fork error\n" END);
 	// return (ERR);
 }
 
