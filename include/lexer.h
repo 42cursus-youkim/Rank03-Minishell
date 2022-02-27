6eb0aa766cafd0a2e16ee0613347fc40b77a95b5
hyojekim <42.4.hyojekim@gmail.com>
2022-02-27 11:55:04 +0900

    refactor: :recycle: Move scanner functions to scanner package
    lexer package에 있던 scanner 함수들을 scanner package로 이동
    
    #85

--- include/lexer.h
+++ include/lexer.h
@@ -1,38 +1,6 @@
 #ifndef LEXER_H
 # define LEXER_H
 
-typedef struct s_scan_data
-{
-	char	*buf;
-	char	*line;
-	int		idx;
-	t_dict	*env;
-	bool	env_flag;
-}	t_scan_data;
-
-typedef struct s_expansion_scan_info
-{
-	t_scan_data		*data;
-	int				start_i;
-	t_AST_expansion	**expansions;
-	int				begin;
-	int				end;
-
-}	t_expansion_scan_info;
-
-typedef struct s_metastr
-{
-	char	*str;
-	char	*prev;
-	int		i;
-}	t_metastr;
-
-typedef struct s_scan_node
-{
-	char				*text;
-	t_AST_expansion		**expansions;
-}	t_scan_node;
-
 typedef struct s_token
 {
 	t_AST_type			type;
@@ -42,58 +10,9 @@ typedef struct s_token
 
 //@func
 /*
-** < dollar_scan1.c > */
-
-t_res		dollar_scan(t_list **list, t_scan_data *data);
-/*
-** < dollar_scan2.c > */
-
-t_res		expansions_update_with_brace( t_expansion_scan_info *info, int end,
-				bool brace);
-t_res		expansion_location_init(t_expansion_scan_info *info, int *i);
-bool		is_multi_expansions(t_expansion_scan_info info, int i);
-/*
-** < expansion.c > */
-
-int			expansions_len(t_AST_expansion *expansions[]);
-t_res		expansions_append(t_AST_expansion **parr[],
-				t_AST_expansion *element);
-t_res		expansions_append_free( t_AST_expansion **parr[],
-				t_AST_expansion *element);
-void		expansions_print(t_AST_expansion *expansions[]);
-/*
 ** < lexer.c > */
 
 t_token		*lexer(char *line, t_dict *env);
-/*
-** < metachar_scan1.c > */
-
-t_res		metachar_scan(t_list **list, t_scan_data *data);
-/*
-** < metachar_scan2.c > */
-
-t_res		metachar_attachable(char **pstr, char prev_c, char c);
-t_res		metastr_init(t_metastr *metastr, char c);
-bool		is_prev_metachar_attachable(char *str);
-/*
-** < scanner.c > */
-
-t_res		scanner(t_list **scan_list, char *line, t_dict *env);
-/*
-** < scanner_list.c > */
-
-t_scan_node	*new_scan_node(char *str, t_AST_expansion **arr);
-void		del_scan_node(void *param);
-void		scan_node_print(void *param);
-/*
-** < scanner_util.c > */
-
-bool		is_quotes_open(char *last_quote, char *str);
-bool		is_brace_open(char *str);
-t_res		list_element_create( t_list **element, char *buf,
-				t_AST_expansion **expansions);
-t_res		buf_to_list(t_list **list, char **buf);
-t_res		whitespace_scan(t_list **list, t_scan_data *data);
 /*
 ** < tokenizer.c > */
 
@@ -108,20 +27,4 @@ t_token		*tokenizer(t_list *scan_list);
 t_AST_type	tokentype_check(t_scan_node *node);
 void		tokens_print(t_token tokens[]);
 void		del_tokens(t_token tokens[]);
-/*
-** < util.c > */
-
-bool		is_metachar(char c);
-bool		is_whitespace(char c);
-bool		is_quotechar(char c);
-bool		is_alpha(char c);
-bool		is_digit(char c);
-/*
-** < util2.c > */
-
-bool		is_1stchar_valid(char c);
-bool		is_variable_char_valid(char c);
-bool		is_scan_continuos(char *buf, char c);
-t_res		free_n_return(char **str, t_res result);
-t_res		free_arr_n_return(char *arr[], t_res result);
 #endif
--- makefile
+++ makefile
@@ -18,12 +18,11 @@ VFLAGS   := --leak-check=full --show-leak-kinds=all \
 HGEN     := hgen
 
 # ===== Packages =====
-PKGS     := prompt lexer parser exec api builtin tree errormsg
+PKGS     := prompt lexer scanner parser exec api builtin tree errormsg
 
-lexerV   := lexer expansion util util2 \
-			scanner scanner_list scanner_util \
-			dollar_scan1 dollar_scan2 metachar_scan1 metachar_scan2 \
-			tokenizer tokenizer_util
+scannerV := scanner scanner_list expansion util util2 util3 \
+			dollar_scan1 dollar_scan2 metachar_scan1 metachar_scan2
+lexerV   := lexer tokenizer tokenizer_util
 parserV  := parser new1 new2 del util1 util2 expander1 expander2
 promptV  := prompt interrupt util
 apiV     := shell redirect signal path file util env1 env2
