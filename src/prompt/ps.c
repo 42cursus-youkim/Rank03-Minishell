252a9fd2c7daa21886fd4ef4147c38b49ce94b0a
scarf005 <greenscarf005@gmail.com>
2022-02-27 18:53:29 +0900

    feat: :sparkles: Prompt colors, green=OK, red=ERROR

---
+++ src/prompt/ps.c
@@ -0,0 +1,18 @@
+#include "minishell.h"
+
+static char	*prompt_color_result(t_shell *shell)
+{
+	static char	*colors[2] = {BRED, BGRN};
+	char		*color;
+
+	color = colors[shell->env->exitcode == OK];
+	return (new_str_join((char *[]){
+			BBLU, shell->prompt.user, "@minishell ",
+			color, "> " END, NULL}, '\0'));
+}
+
+void	prompt_refresh_ps(t_shell *shell)
+{
+	shell->prompt.user = env_get(shell->env, "USER");
+	ft_str_replace(&shell->prompt.ps1, prompt_color_result(shell));
+}
