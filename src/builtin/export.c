80f5f75e9c8e1fe96b9b4cd5d910aca68b9b12c8
scarf005 <greenscarf005@gmail.com>
2022-02-28 17:49:04 +0900

    feat: :sparkles: Exoport without args or value

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -1,13 +1,33 @@
 #include "minishell.h"
 
-int	builtin_export(t_context *context, t_shell *shell)
+static int	export_print(t_dict *env)
+{
+	int		id;
+	t_ditem	*item;
+	t_ditem	**items;
+
+	items = new_dict_kv_ordered(env);
+	id = -1;
+	while (++id < env->size)
+	{
+		item = items[id];
+		if (!item || !item->key)
+			continue ;
+		printf(BHYEL "declare -x %-s" END, item->key);
+		if (item->value)
+			printf("=" HGRN "%s" END, (char *)item->value);
+		printf("\n");
+	}
+	free(items);
+	return (EXIT_SUCCESS);
+}
+
+static int	export_variables(
+	t_context *context, t_shell *shell, const int len)
 {
 	int			i;
 	t_res		res;
-	const int	len = ft_arr_len(context->argv);
 
-	if (check_no_opt(context->argv, "export") != OK)
-		return (EXIT_FAILURE);
 	res = OK;
 	i = 0;
 	while (++i < len)
@@ -15,4 +35,16 @@ int	builtin_export(t_context *context, t_shell *shell)
 	if (res != OK)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
+}
+
+int	builtin_export(t_context *context, t_shell *shell)
+{
+	const int	len = ft_arr_len(context->argv);
+
+	if (check_no_opt(context->argv, "export") != OK)
+		return (EXIT_FAILURE);
+	if (len == 1)
+		return (export_print(shell->env));
+	else
+		return (export_variables(context, shell, len));
 }
