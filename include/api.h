71dbb9ca8c540b1d4bb988d8418b69374bbf3298
scarf005 <greenscarf005@gmail.com>
2022-02-28 13:56:45 +0900

    fix: :bug: Error check when command is directory

--- include/api.h
+++ include/api.h
@@ -25,7 +25,8 @@ char	*env_get(t_dict *env, char *key);
 /*
 ** < file.c > */
 
-bool	is_file_exists(char *filename);
+bool	is_dir(char *path);
+bool	is_file_exists(char *path);
 bool	is_executable_exists(char *file, t_dict *env);
 char	*new_executable_from_env(char *file, t_dict *env);
 /*
--- src/api/file.c
+++ src/api/file.c
@@ -1,10 +1,19 @@
 #include "minishell.h"
 
-bool	is_file_exists(char *filename)
+bool	is_dir(char *path)
+{
+	struct stat	statbuf;
+
+	if (stat(path, &statbuf) == ERR)
+		return (false);
+	return (S_ISDIR(statbuf.st_mode));
+}
+
+bool	is_file_exists(char *path)
 {
 	struct stat	buffer;
 
-	return (stat(filename, &buffer) == OK);
+	return (stat(path, &buffer) == OK);
 }
 
 bool	is_executable_exists(char *file, t_dict *env)
@@ -31,7 +40,12 @@ char	*new_executable_from_env(char *file, t_dict *env)
 	char	**path;
 	char	*executable;
 
-	if (is_file_exists(file))
+	if (is_file_exists(file) && is_executable_exists(file, env))
+	{
+		error_msg_category("is a directory", file);
+		return (NULL);
+	}
+	else if (is_file_exists(file))
 		return (new_str(file));
 	else if (!is_executable_exists(file, env))
 		return (NULL);
