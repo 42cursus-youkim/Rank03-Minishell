015b34bd9ede9e220882f3fc86de4c5c71784bce
scarf005 <greenscarf005@gmail.com>
2022-03-01 14:50:30 +0900

    fix: :bug: Handle exit with arguments properly

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -1,22 +1,46 @@
 #include "minishell.h"
 
+static int	check_args(t_context *context)
+{
+	int			arg_idx;
+	const int	argv_len = ft_arr_len(context->argv);
+
+	arg_idx = 0;
+	while (++arg_idx <= argv_len)
+	{
+		if (!is_str_digit(context->argv[arg_idx]))
+		{
+			error_msg_return((char *[]){"exit: ", context->argv[arg_idx],
+				": numeric argument required", NULL});
+			return (EXIT_EXIT_STATUS_OUT_OF_RANGE);
+		}
+		else if (argv_len > 2)
+		{
+			error_msg_category("exit", "too many arguments");
+			return (EXIT_FAILURE);
+		}
+	}
+	return (OK);
+}
+
 //	FIXME: needs to work in pipe too
 int	builtin_exit(t_context *context, t_shell *shell)
 {
+	int			exitcode;
 	int			exitcode_input;
-	const int	argv_len = ft_arr_len(context->argv);
 
-	del_context(context);
-	if (argv_len > 2)
-	{
-		error_msg_category("exit", "too many arguments");
-		return (EXIT_FAILURE);
-	}
+	exitcode = check_args(context);
+	if (exitcode == EXIT_EXIT_STATUS_OUT_OF_RANGE)
+		;
+	else if (exitcode == EXIT_FAILURE)
+		return (exitcode);
 	else if (check_no_opt(context->argv, "exit") != OK)
-		api_exit(shell, EXIT_EXIT_STATUS_OUT_OF_RANGE);
+		exitcode = EXIT_EXIT_STATUS_OUT_OF_RANGE;
 	else if (ft_atoi(context->argv[1], &exitcode_input) == OK)
-		api_exit(shell, exitcode_input % EXIT_CODE_RANGE);
+		exitcode = exitcode_input % EXIT_CODE_RANGE;
 	else
-		api_exit(shell, shell->env->exitcode);
+		exitcode = shell->env->exitcode;
+	del_context(context);
+	api_exit(shell, exitcode);
 	return (EXIT_FAILURE);
 }
