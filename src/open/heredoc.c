f2c8f24fe2525bd4d80064d281c7be2ba4874548
scarf005 <greenscarf005@gmail.com>
2022-03-02 11:37:50 +0900

    fix: :bug: Fix heredoc segfault

--- src/open/heredoc.c
+++ src/open/heredoc.c
@@ -7,13 +7,22 @@ static void	eof_heredoc(t_shell *shell)
 	ft_write(1, shell->prompt.ps2);
 }
 
+static	t_res	write_to_heredoc_and_free(t_fd fd, char *str)
+{
+	int	res;
+
+	res = ft_writes(fd, (char *[]){str, "\n", NULL});
+	ft_free(str);
+	if (res == ERR)
+		return (ERR);
+	return (OK);
+}
+
 static t_res	process_heredoc(t_shell *shell, t_fd fd, char **pstr)
 {
 	if (replace_line_heredoc(pstr, shell->env) == ERR)
 		return (ERR);
-	ft_writes(fd, (char *[]){*pstr, "\n", NULL});
-	free(*pstr);
-	return (OK);
+	return (write_to_heredoc_and_free(fd, *pstr));
 }
 
 /*	get input for heredoc
@@ -36,9 +45,13 @@ t_fd	shell_heredoc(t_shell *shell, const char *eof)
 		}
 		else if (is_str_equal(line, eof))
 			break ;
-		else
-			if (process_heredoc(shell, pipefd[PIPE_WRITE], &line) == ERR)
-				break ;
+		else if (is_str_equal(line, ""))
+		{
+			write_to_heredoc_and_free(pipefd[PIPE_WRITE], line);
+			break ;
+		}
+		else if (process_heredoc(shell, pipefd[PIPE_WRITE], &line) == ERR)
+			break ;
 	}
 	if (line)
 		free(line);
