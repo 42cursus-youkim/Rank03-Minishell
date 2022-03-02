4d6ee4a3b4485e5a10b58a7b62d742fce48a09b9
scarf005 <greenscarf005@gmail.com>
2022-03-02 11:49:39 +0900

    fix: :bug: Fixed #127

--- src/open/heredoc.c
+++ src/open/heredoc.c
@@ -1,12 +1,5 @@
 #include "minishell.h"
 
-// ctrl + d in heredoc readline
-static void	eof_heredoc(t_shell *shell)
-{
-	cursor_up();
-	ft_write(1, shell->prompt.ps2);
-}
-
 static	t_res	write_to_heredoc_and_free(t_fd fd, char *str)
 {
 	int	res;
@@ -25,36 +18,35 @@ static t_res	process_heredoc(t_shell *shell, t_fd fd, char **pstr)
 	return (write_to_heredoc_and_free(fd, *pstr));
 }
 
+static void	shell_heredoc_loop(
+	t_shell *shell, t_fd pipefd[PIPE_SIZE], const char *eof)
+{
+	char	*line;
+
+	while (true)
+	{
+		line = readline(shell->prompt.ps2);
+		if (!line)
+			return (prompt_eof_heredoc(shell));
+		else if (is_str_equal(line, eof))
+			return (ft_free(line));
+		else if (is_str_equal(line, ""))
+			write_to_heredoc_and_free(pipefd[PIPE_WRITE], line);
+		else if (process_heredoc(shell, pipefd[PIPE_WRITE], &line) == ERR)
+			return ;
+	}
+}
+
 /*	get input for heredoc
 	returns opened fd that points to it
 */
 t_fd	shell_heredoc(t_shell *shell, const char *eof)
 {
-	char	*line;
 	t_fd	pipefd[PIPE_SIZE];
 
 	if (pipe(pipefd) == ERR)
 		return (ERR);
-	while (true)
-	{
-		line = readline(shell->prompt.ps2);
-		if (!line)
-		{
-			eof_heredoc(shell);
-			break ;
-		}
-		else if (is_str_equal(line, eof))
-			break ;
-		else if (is_str_equal(line, ""))
-		{
-			write_to_heredoc_and_free(pipefd[PIPE_WRITE], line);
-			break ;
-		}
-		else if (process_heredoc(shell, pipefd[PIPE_WRITE], &line) == ERR)
-			break ;
-	}
-	if (line)
-		free(line);
+	shell_heredoc_loop(shell, pipefd, eof);
 	close(pipefd[PIPE_WRITE]);
 	return (pipefd[PIPE_READ]);
 }
