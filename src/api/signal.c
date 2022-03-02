539b7fbe2464f06fcc240de9776aa7125cdb24e1
scarf005 <greenscarf005@gmail.com>
2022-03-02 18:04:07 +0900

    fix: :bug: Change to stderr

--- src/api/signal.c
+++ src/api/signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyojekim  <hyojekim@student.42seoul.k      +#+  +:+       +#+        */
+/*   By: youkim <youkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 16:16:52 by hyojekim          #+#    #+#             */
-/*   Updated: 2022/03/02 16:16:52 by hyojekim         ###   ########.fr       */
+/*   Updated: 2022/03/02 18:03:48 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	api_handle_signaled_status(int status)
 	const int	sig = WTERMSIG(status);
 
 	if (sig == SIGQUIT)
-		ft_write(1, HRED "QUIT: 3\n" END);
+		ft_write(STDERR_FILENO, BHRED "QUIT: 3\n" END);
 	return (EXIT_FATAL_ERR_CAUSED_BY_SIGNAL + sig);
 }
 
